b78759596d1c7c9e2388114b1fa1841895c46a4c
JulAgu <jsagudeloac@unal.edu.co>
2023-10-13 11:24:08 +0200

    deleted test.txt, add of test.sh (script bash having as goal to do all the process)

---
+++ test.sh
@@ -0,0 +1,5 @@
+#fasterq-dump --threads <#CPUS> --progress <SRAID>
+#gzip *.fastq
+wget -q -O ~/ReproHackaton_2023/ressources/genomes/reference.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=CP000253.1&rettype=fasta"
+wget -O ~/ReproHackaton_2023/ressources/genomes/reference.gff "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&report=gff3&id=CP000253.1"
+#trim_galore -q 20 --phred33 --length 25 <FASTQ FILE>

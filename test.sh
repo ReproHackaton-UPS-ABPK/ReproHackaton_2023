bfafccaeb12d656ed29d9656203265cc46cf2ccd
JulAgu <jsagudeloac@unal.edu.co>
2023-10-19 17:56:03 +0200

    I add the Sra-toolkit Instalation .tar.bz2 for linux machines

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#fasterq-dump --threads 4 --progress GSM4145666
+fasterq-dump SRP227811
 #gzip *.fastq
 #wget -q -O ~/ReproHackaton_2023/ressources/genomes/reference.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=CP000253.1&rettype=fasta"
 #wget -O ~/ReproHackaton_2023/ressources/genomes/reference.gff "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&report=gff3&id=CP000253.1"

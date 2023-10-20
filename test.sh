18fa6914927787537bf6d6bb12c326f91291f3af
Nada Kassara <kassaranada@gmail.com>
2023-10-20 12:34:14 +0200

    Modifications qui seront validées : 	modifié :         test.sh

--- test.sh
+++ test.sh
@@ -12,14 +12,13 @@
 #wget -O ~/ReproHackaton_2023/ressources/genomes/reference.gff "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&report=gff3&id=CP000253.1"
 
 #---------- CRÉATION DE L'INDEX ---------------------
-
 #bowtie2-build reference.fasta aureusIndex
 
 #---------- TRIMMING : suppression des gènes indésirables ---------------------
-#trim_galore -q 20 --phred33 --length 25 <fichier.fastq.gz>
+#trim_galore -q 20 --phred33 --length 25 SRR10379726.fastq.gz
 
 #---------- MAPPING ---------------------
-#bowtie2 -x aureusIndex -U <fichier.fastq.gz> -S test.sam
+#bowtie2 -x aureusIndex -U SRR10379726_trimmed.fq.gz -S SRR10379726_test.sam
 
 #---------- COUNTING (SUBREAD) ---------------------
 #featureCounts -p -O -T n -a <example_genome_annotation.gtf> -o fichier_output.txt <sorted_example_alignment.bam>
